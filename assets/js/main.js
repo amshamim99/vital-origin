86878c6a5283e8ca161705994b71a6e396d01b7c
shamim <amshamim99@gmail.com>
2024-11-01 08:52:56 +0600

    mobile menu added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,62 @@
 (function ($) {
     "use strict";
 
+    // mobile menu//
+	var tpMenuWrap = $('.tp-mobile-menu-active > ul').clone();
+	var tpSideMenu = $('.tp-offcanvas-menu nav');
+	tpSideMenu.append(tpMenuWrap);
+	if ($(tpSideMenu).find('.tp-submenu, .tp-mega-menu').length != 0) {
+		$(tpSideMenu).find('.tp-submenu, .tp-mega-menu').parent().append
+			('<button class="tp-menu-close"><i class="fi-chevron-right"></i></button>');
+	}
+	var sideMenuList =
+		$('.tp-offcanvas-menu nav > ul > li button.tp-menu-close, .tp-offcanvas-menu nav > ul li.has-dropdown > a');
+	$(sideMenuList).on('click', function (e) {
+		e.preventDefault();
+		if (!($(this).parent().hasClass('active'))) {
+			$(this).parent().addClass('active');
+			$(this).siblings('.tp-submenu, .tp-mega-menu').slideDown();
+		} else {
+			$(this).siblings('.tp-submenu, .tp-mega-menu').slideUp();
+			$(this).parent().removeClass('active');
+		}
+	});
+
+	// offcanvas
+	$(".tp-offcanvas-open-btn").on("click", function () {
+		$(".tp-offcanvas-area").addClass("opened");
+		$(".body-overlay").addClass("opened");
+	});
+	$(".tp-offcanvas-close-btn").on("click", function () {
+		$(".tp-offcanvas-area").removeClass("opened");
+		$(".body-overlay").removeClass("opened");
+	});
+
+	// // 09. Body overlay Js
+	$(".body-overlay").on("click", function () {
+		$(".tp-offcanvas-area").removeClass("opened");
+		$(".body-overlay").removeClass("opened");
+	});
+    // mobile-menu-end//
+
+
+    // menu-fixed-top///
+    if($('.main-header, .main-header-tow').length > 0){
+        var fixed_top = $(".main-header, .main-header-tow");
+        $(window).on("scroll", function() {
+        if ($(this).scrollTop() > 20) {
+            fixed_top.addClass("menu-fixed animated fadeInDown");
+            fixed_top.removeClass("slideInUp");
+            $("body").addClass("body-padding");
+        }else {
+            fixed_top.removeClass("menu-fixed fadeInDown");
+            fixed_top.addClass("slideInUp");
+            $("body").removeClass("body-padding");
+        }
+        });
+    }
+
+    // slide-active
     $('.slide-active').slick({
         infinite: true,
         arrows:false,
